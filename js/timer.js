8d5f2134dcaf552717230d45de249caa3878f429
Sam <2261522g@student.gla.ac.uk>
2019-06-21 00:38:58 +0100

    basic page, no javascript working yet

---
+++ js/timer.js
@@ -0,0 +1,18 @@
+
+
+var date_meeting = new Date("Aug 24, 2019 08:00:00").getTime();
+
+var x = setInterval(
+	function() {
+		var current_time = new Date().getTime();
+
+
+		dist = date_meeting-current_time
+
+		var days = Math.floor(distance / (1000*60*60*24));
+		var hours = Math.floor((distance / (1000*60*60*24)) 
+		var minutes = 
+		var seconds = 
+
+
+)
