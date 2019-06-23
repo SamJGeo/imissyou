77af2edc8a249bac44ecd5af97785664c734a5c7
Sam <2261522g@student.gla.ac.uk>
2019-06-23 23:28:52 +0100

    timer function working

--- js/timer.js
+++ js/timer.js
@@ -1,18 +1,41 @@
 
 
-var date_meeting = new Date("Aug 24, 2019 08:00:00").getTime();
 
-var x = setInterval(
-	function() {
-		var current_time = new Date().getTime();
-
-
-		dist = date_meeting-current_time
-
-		var days = Math.floor(distance / (1000*60*60*24));
-		var hours = Math.floor((distance / (1000*60*60*24)) 
-		var minutes = 
-		var seconds = 
+function timer(){
+	var date_meeting = new Date("Aug 24, 2019 08:00:00").getTime();
+	var current_time = new Date().getTime();
 
 
-)
+	dist = date_meeting-current_time
+
+	var days = Math.floor(dist / (1000*60*60*24));
+	var hours = Math.floor((dist % (1000*60*60*24)) / (1000*60*60)); 
+	var minutes = Math.floor((dist % (1000*60*60)) / (1000*60));
+	var seconds = Math.floor((dist % (1000*60)) / 1000);
+
+	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+}
+
+
+function arrived(){
+	document.getElementById("countdown").innerHTML="IM COMIN BABY";
+}
+
+
+function checker(){
+	var date_meeting = new Date("Aug 24, 2019 08:00:00").getTime();
+	var current_time = new Date().getTime();
+
+
+	dist = date_meeting-current_time
+
+	if (dist < 0){
+		return true;
+	} else {
+		return false;
+	}
+
+
+
+}
