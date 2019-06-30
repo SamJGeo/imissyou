b613c8a948200b425cada416e0a052b7ccc4d0f0
Sam <2261522g@student.gla.ac.uk>
2019-06-30 21:53:09 +0100

    grid addition and images

--- js/timer.js
+++ js/timer.js
@@ -13,7 +13,7 @@ function timer(){
 	var minutes = Math.floor((dist % (1000*60*60)) / (1000*60));
 	var seconds = Math.floor((dist % (1000*60)) / 1000);
 
-	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+	document.getElementById("countdown").innerHTML = days + " : " + hours + " : " + minutes + " : " + seconds;
 
 }
 
