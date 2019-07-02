0416f49fc4557721a94d851e0575050477fbd83f
Sam <2261522g@student.gla.ac.uk>
2019-07-02 06:21:14 +0100

    colour and spacing

--- js/timer.js
+++ js/timer.js
@@ -13,6 +13,22 @@ function timer(){
 	var minutes = Math.floor((dist % (1000*60*60)) / (1000*60));
 	var seconds = Math.floor((dist % (1000*60)) / 1000);
 
+	if (days<10){
+		days = "0"+days
+	}
+
+	if (hours<10){
+		hours = "0"+hours
+	}
+
+	if (minutes<10){
+		minutes = "0"+minutes
+	}
+
+	if (seconds<10){
+		seconds = "0"+seconds
+	}
+
 	document.getElementById("countdown").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
 
 }
