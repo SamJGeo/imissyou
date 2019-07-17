bce957f440556818536791b8fbc36291cab583d5
Sam <2261522g@student.gla.ac.uk>
2019-07-17 21:52:38 +0100

    messaging and formatting

--- js/button.js
+++ js/button.js
@@ -1,19 +1,28 @@
-function swicth_button(){
+function switch_button(){
 
-    var p1 =getElementByClassName("photo_one");
-    var p2 =getElementByClassName("photo_two");
-    var message = getElementByClassName("photo_desc");
+    var p1 =document.getElementsByClassName("photo_one")[0];
+    var p2 =document.getElementsByClassName("photo_two")[0];
+    var message = document.getElementsByClassName("main_button")[0];
+    var note = document.getElementsByClassName("note_wrapper")[0];
 
 
 
     if (p1.style.display === "none"){
+        console.log(p1)
+
         p1.style.display = "block";
         p2.style.display = "block";
-        message.innerHTML = "Photos Of The Week:";
+        note.style.display = "none";
+
+        message.innerHTML = "Show Note";
     } else {
+        console.log(p1)
+
         p1.style.display = "none";
         p2.style.display = "none";
-        message.innerHTML = "A Wee Note For You:";
+        note.style.display = "block"
+
+        message.innerHTML = "Show Photos";
 
     }
 }
